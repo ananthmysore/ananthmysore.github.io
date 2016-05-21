0e1a04e80f0863eaa87d7db77403a8a3b8bcb68e
ananthmysore <ananthj87@gmail.com>
2016-05-21 19:15:48 +0530

    changed to madam's site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,100 +1,434 @@
-/*
-	Alpha by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+/*jshint jquery:true */
+/*global $:true */
 
-(function($) {
+var $ = jQuery.noConflict();
 
-	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 980px)',
-		narrower: '(max-width: 840px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
+$(document).ready(function($) {
+    "use strict";
+
+    /* global google: false */
+
+    /* ==============================================
+        Full height home-section
+    =============================================== */
+
+	var windowHeight = $(window).height(),
+		topSection = $('#hero-section');
+	topSection.css('height', windowHeight);
+
+	$(window).resize(function(){
+		var windowHeight = $(window).height();
+		topSection.css('height', windowHeight);
 	});
 
-	$(function() {
+    /* ==============================================
+        Collapse menu on click
+    =============================================== */
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
+        $('.navbar-collapse a:not(.dropdown-toggle)').click(function(){
+            if($(window).width() < 768 )
+                $('.navbar-collapse').collapse('hide');
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+    /* ==============================================
+        Scrollspy
+    =============================================== */
 
-		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});
+        $('body').scrollspy({
+           target: '#navigation-nav',
+           offset: 140      //px/
+        });
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				alignment: 'right'
-			});
+    /* ==============================================
+        Parallax
+    =============================================== */
 
-		// Off-Canvas Navigation.
+    $.stellar({
+        responsive: true,
+        horizontalScrolling: false,
+        verticalOffset: 0
+    });
 
-			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+    /* ==============================================
+        Hero slider
+    =============================================== */
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+    // $('.caption-slides').bxSlider({
+    //   pager: false,
+    //   mode: 'fade',
+    //   adaptiveHeight: true,
+    //   controls: false,
+    //   auto: true
+    // });
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');
+    /* ==============================================
+        Smooth Scroll on anchors
+    =============================================== */
 
-		// Header.
-		// If the header is using "alt" styling and #banner is present, use scrollwatch
-		// to revert it back to normal styling once the user scrolls past the banner.
-		// Note: This is disabled on mobile devices.
-			if (!skel.vars.mobile
-			&&	$header.hasClass('alt')
-			&&	$banner.length > 0) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+                  scrollTop: target.offset().top -66
+            }, 1000);
+            return false;
+          }
+        }
+    });
 
-				$window.on('load', function() {
+    /* ==============================================
+     Bootstrap Tooltip
+    =============================================== */
 
-					$banner.scrollwatch({
-						delay:		0,
-						range:		0.5,
-						anchor:		'top',
-						on:			function() { $header.addClass('alt reveal'); },
-						off:		function() { $header.removeClass('alt'); }
-					});
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 
-				});
+    /* ==============================================
+        Counter increment
+    =============================================== */
 
-			}
+    function countUp() {
+        var dataperc;
+        $('.statistic-percent').each(function(){
+            dataperc = $(this).attr('data-perc'),
+            $(this).find('.percentfactor').delay(6000).countTo({
+                from: 0,                 // number to begin counting
+                to: dataperc,
+                speed: 1000,             // ms
+                refreshInterval: 10,
+            });
+        });
+    }
 
-	});
+    $('.statistic-percent').waypoint(function() {
+        countUp();
+    },
+    {
+        offset: '95%',
+        triggerOnce: true
+    });
 
-})(jQuery);
+    /* ==============================================
+        Skills bar
+    =============================================== */
+
+        $('.progress-bar').each(function(i) {
+            $(this).appear(function() {
+                var percent = $(this).attr('aria-valuenow');
+                $(this).animate({'width' : percent + '%'});
+            });
+        });
+
+    /* ==============================================
+    Placeholder
+    =============================================== */
+
+    $('input, textarea').placeholder();
+
+    /* ==============================================
+        Animated content
+    =============================================== */
+
+    $('.animated').appear(function(){
+        var el = $(this);
+        var anim = el.data('animation');
+        var animDelay = el.data('delay');
+        if (animDelay) {
+
+            setTimeout(function(){
+                el.addClass( anim + " in" );
+                el.removeClass('out');
+            }, animDelay);
+
+        }
+
+        else {
+            el.addClass( anim + " in" );
+            el.removeClass('out');
+        }
+    },{accY: -150});
+
+
+    /* ==============================================
+        MailChip
+    =============================================== */
+
+    $('.mailchimp').ajaxChimp({
+        callback: mailchimpCallback,
+        url: "http://clas-design.us10.list-manage.com/subscribe/post?u=5ca5eb87ff7cef4f18d05e127&amp;id=9c23c46672" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".
+    });
+
+    function mailchimpCallback(resp) {
+         if (resp.result === 'success') {
+            $('.subscription-success').html('<span class="icon-happy"></span><br/>' + resp.msg).fadeIn(1000);
+            $('.subscription-error').fadeOut(500);
+
+        } else if(resp.result === 'error') {
+            $('.subscription-error').html('<span class="icon-sad"></span><br/>' + resp.msg).fadeIn(1000);
+            $('.subscription-success').fadeOut(500);
+        }
+    }
+
+
+    /* ==============================================
+    Contact Form
+    =============================================== */
+
+    $('#contactform').submit(function(){
+
+        var action = $(this).attr('action');
+
+        $("#alert").slideUp(750,function() {
+            $('#alert').hide();
+
+        $('#submit')
+            .after('<img src="assets/images/ajax-loader.GIF" class="contactloader" />')
+            .attr('disabled','disabled');
+
+        $.post(action, {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+        },
+            function(data){
+                document.getElementById('alert').innerHTML = data;
+                $('#alert').slideDown('slow');
+                $('#contactform img.contactloader').fadeOut('slow',function(){$(this).remove();});
+                $('#submit').removeAttr('disabled');
+                if(data.match('success') !== null) {
+                    $('#name').val('');
+                    $('#email').val('');
+                    $('#message').val('');
+                }
+            }
+        );
+
+        });
+
+        return false;
+
+    });
+
+    // Countdown
+    // To change date, simply edit: var endDate = "June 26, 2015 20:39:00";
+    $(function() {
+      var endDate = "June 26, 2016 20:39:00";
+      $('.lj-countdown .row').countdown({
+        date: endDate,
+        render: function(data) {
+          $(this.el).html('<div><div><span>' + (parseInt(this.leadingZeros(data.years, 2)*365) + parseInt(this.leadingZeros(data.days, 2))) + '</span><span>days</span></div><div><span>' + this.leadingZeros(data.hours, 2) + '</span><span>hours</span></div></div><div class="lj-countdown-ms"><div><span>' + this.leadingZeros(data.min, 2) + '</span><span>minutes</span></div><div><span>' + this.leadingZeros(data.sec, 2) + '</span><span>seconds</span></div></div>');
+        }
+      });
+    });
+
+
+    /* ==============================================
+    Fade In .back-to-top
+    =============================================== */
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+
+    // scroll body to 0px on click
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0,
+            easing: 'swing'
+        }, 750);
+        return false;
+    });
+
+    /* ==============================================
+        Google Map
+    =============================================== */
+
+        var mapLocation = new google.maps.LatLng(13.019953, 77.562447);
+        var $mapis = $('#map');
+        if ($mapis.length > 0) {
+            var map;
+            map = new GMaps({
+                streetViewControl : true,
+                overviewMapControl: true,
+                mapTypeControl: true,
+                zoomControl : true,
+                panControl : true,
+                scrollwheel: false,
+                center: mapLocation,
+                el: '#map',
+                zoom: 18,
+                styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+            });
+            var image = new google.maps.MarkerImage('assets/images/map-icon.png');
+            map.addMarker({
+                position: mapLocation,
+                icon: image,
+                title: 'Visia',
+            });
+        }
+
+    /* ==============================================
+     BX-Project Slider
+    =============================================== */
+
+        $(".project-slider").bxSlider({
+            pager: false,
+            controls: true,
+            auto: true,        // Boolean:  (true/false)
+            speed: 500,        // Animation speed.
+            pause: 5000,      // Milliseconds before progressing to next slide automatically. Use a falsey value to disable.
+            useCSS: false     // Boolean:  (true/false)
+        });
+
+    /* ==============================================
+     BX-Project Slider
+    =============================================== */
+
+        $(".blog-slider").bxSlider({
+            pager: false,
+            controls: true,
+            auto: true,        // Boolean:  (true/false)
+            speed: 500,        // Animation speed.
+            pause: 5000,      // Milliseconds before progressing to next slide automatically. Use a falsey value to disable.
+            useCSS: false     // Boolean:  (true/false)
+        });
+
+    /* ==============================================
+     BX-Slider Tweet&Process
+    =============================================== */
+
+
+        $('.tweet-slider').bxSlider({
+          adaptiveHeight: true,
+          controls: false,
+          auto: true
+        });
+
+    /* ==============================================
+        BxSlider Testimonial
+    =============================================== */
+
+    $(".testimonials-slider").bxSlider({
+        nextSelector: ".tc-arrows .tc-arrow-right",
+        prevSelector: ".tc-arrows .tc-arrow-left",
+        nextText: "<i class='fa fa-angle-right'></i>",
+        prevText: "<i class='fa fa-angle-left'></i>",
+        pager: false,
+        auto: true,          // Boolean:  (true/false)
+        pause: 5000,         // Milliseconds before progressing to next slide automatically. Use a falsey value to disable.
+        mode: 'vertical',    // Choose fade, slide
+        useCSS: false        // Boolean:  (true/false)
+    });
+
+    /* ==============================================
+        OWL Carousel
+    =============================================== */
+
+        $(".owl-carousel").owlCarousel({
+
+            autoPlay: 3000, //Set AutoPlay to 3 seconds
+            items : 4,
+            itemsDesktop : [1199,3], //number of items displayed on resolution less then 1199px
+            itemsDesktopSmall : [979,3] //number of items displayed on resolution less then 979px
+
+        });
+
+    /* ==============================================
+        Responsive video
+    =============================================== */
+
+        $(".project-video, .video-creative, .video-post").fitVids();
+
+
+    /* ==============================================
+        MagnificPopup - lightbox effect
+    =============================================== */
+
+        // Example with multiple objects
+        $('.zoom').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+
+        $('.video-pop-up').magnificPopup({
+            type: 'iframe',
+        });
+
+    /* ==============================================
+        OWL Carousel (initialize screenshot carousel)
+    =============================================== */
+
+    $(".screenshots-carousel").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 5,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+
+    });
+
+});
+
+$(window).load(function(){
+    "use strict";
+
+    /* ==============================================
+    Isotope
+    =============================================== */
+
+        // FIlter
+        if( $("#filter").length>0 ) {
+            var container = $('#filter');
+            container.isotope({
+                itemSelector: '.gallery-item',
+                transitionDuration: '0.8s'
+            });
+            $(".filter").click(function(){
+                $(".filter.active").removeClass("active");
+                $(this).addClass("active");
+                var selector = $(this).attr('data-filter');
+                container.isotope({
+                    filter: selector
+                });
+                return false;
+            });
+
+            $(window).resize(function(){
+                setTimeout(function(){
+                    container.isotope();
+                },1000);
+            }).trigger('resize');
+        }
+
+
+            if ( $('#type-masory').length ) {
+
+            var $container = $('#type-masory');
+
+            $container.imagesLoaded( function(){
+              $container.fadeIn(1000).isotope({
+                itemSelector : '.masonry-item'
+              });
+            });
+        }
+
+    /* ==============================================
+    Preloader
+    =============================================== */
+
+    // will first fade out the loading animation
+    $("#loading-animation").fadeOut();
+    // will fade out the whole DIV that covers the website.
+    $("#preloader").delay(600).fadeOut("slow");
+
+});
